375521aa54867951e46c50f7e90fd0645bb6f2ba
Feiyu Zheng <feiyuzheng98@gmail.com>
2022-05-27 16:32:44 -0400

    Update

--- bot.py
+++ bot.py
@@ -5,31 +5,31 @@ from discord.ext import commands
 import json
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-import zoneinfo
 
 with open("./bot.json", 'r') as token_file:
     TOKEN = json.load(token_file)['token']
 
 bot = discord.Client()
+testChannelID = 979492901019074631
 
 async def leetcode_start():
     await bot.wait_until_ready()
-    mainChannel = bot.get_channel(978549594877231117)
+    mainChannel = bot.get_channel(testChannelID)
     await mainChannel.send("func")
 
 async def leetcode_end():
     await bot.wait_until_ready()
-    mainChannel = bot.get_channel(978549594877231117)
+    mainChannel = bot.get_channel(testChannelID)
     await mainChannel.send("func")
 
 @bot.event
 async def on_ready():
-    mainChannel = bot.get_channel(978549594877231117)
+    mainChannel = bot.get_channel(testChannelID)
     print(f"{bot.user} has connected to Discord!")
     await mainChannel.send(f"{bot.user.name} has connected to Discord!")
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(func, CronTrigger(hour = "12", minute="0", second="0", timezone="America/New_York"))
+    scheduler.add_job(leetcode_start, CronTrigger(hour = "12", minute="0", second="0", timezone="America/New_York"))
     scheduler.start()
 
 @bot.event
@@ -37,7 +37,17 @@ async def on_message(message):
     if message.author == bot.user:
         return
     
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content[0] == '$':
+        if message.content[1:] == "help":
+            await print_help_message(message.channel)
+        elif message.content[1:] == "quit":
+            await bot.close()
+        elif message.content[1:] == "hello":
+            await message.channel.send(message.author.mention + " hello!") 
+
+async def print_help_message(channel):
+    message = "help message"
+    await channel.send(message)
+
 
 bot.run(TOKEN)
